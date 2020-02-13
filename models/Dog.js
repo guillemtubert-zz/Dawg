2987f3ffd3f14d1b08acb19606c20dad57087245
guillemtubert <guillemtubertsesen@gmail.com>
2020-02-13 18:05:21 +0100

    models

--- models/Dog.js
+++ models/Dog.js
@@ -7,11 +7,16 @@ const dogSchema = new Schema(
   password: { type: String, required: true },
   dogName: { type: String, required: true },
   phoneNumber: { type: Number, required: true },
-  breed: { type: String, enum: ['bulldog', 'shi tzu'], required: true},
-  age: { type: Number, required: true },
+  breed: { type: String, enum: ['bulldog', 'shi tzu','everything'], required: true},
+  age: { type: Number, min: 0, max: 20,required: true },
   image: { type: String, default: "https://i.pinimg.com/originals/fa/6b/7b/fa6b7b4aa80577ecc8cb475922e9227d.jpg" },
   activity: { type: String, enum: ["Shy","Friendly","Very Sociable"], required: true},
   interactions: [matchId],
+  searchPreferences :{
+    breed: { type: String, enum: ['bulldog', 'shi tzu', 'everything'], required: true},
+    ageMin : { type: Number, min: 0, max: 19,required: true },
+    ageMax : { type: Number, min: 1, max: 20,required: true },
+  }
   },
   {
     timestamps: {
