955f2dcb21493a3751a5218754f150c6805e1858
guillemtubert <guillemtubertsesen@gmail.com>
2020-02-15 19:39:49 +0100

    nav bar linked

--- routes/site-routes.js
+++ routes/site-routes.js
@@ -4,15 +4,23 @@ var siteRouter = express.Router();
 // AUTHENTICATION CHECKER
 siteRouter.use((req, res, next) => {
   if (req.session.currentUser) { // <== if there's user in the session (user is logged in)
-    next(); // ==> go to the next route ---
-  } 																//		|
-  else {                          	//    |
-  	res.redirect("/auth/login");       	//    |
-  }                                 //    |
-});																	//		|
+    next(); // ==> go to the next route -->
+  } 																
+  else {                          	
+  	res.redirect("/auth/login");
+  }                     
+});																
 
-// siteRouter.get('/secret',  (req, res) => {
-//   res.render('secret')
-// })
+siteRouter.get('/match',  (req, res) => {
+  res.render('match')
+});
+
+siteRouter.get('/profile',  (req, res) => {
+  res.render('profile')
+});
+
+siteRouter.get('/swipe',  (req, res) => {
+  res.render('swipe')
+});
 
 module.exports = siteRouter;
