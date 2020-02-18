9728ef4e55b51546bd028f60da2382a992cb0455
doveriko <abelmorpe@gmail.com>
2020-02-18 11:12:38 +0100

    delete profile

--- routes/match.js
+++ routes/match.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var matchRouter = express.Router();
 
+const Match = require("../models/Match");
+
 matchRouter.get('/',  (req, res) => {
     res.render('match')
+  });
+
+matchRouter.post('/',  (req, res) => {
+    
+    // Match.create
+
+
   });
 
   module.exports = matchRouter;
--- routes/profile.js
+++ routes/profile.js
@@ -19,21 +19,40 @@ siteRouter.use((req, res, next) => {
 });
 
 siteRouter.get('/profile',  (req, res) => {
-  const {_id} = req.query;
+  const {_id} = req.session.currentUser;
 
   //doc.find by id to 
   Dog.findOne({_id})
   .then( (dogProfile) => {
-
+    console.log(dogProfile);
+    
     const {dogName, age, phoneNumber, breed, image, activity} = dogProfile;
     res.render('profile', {dogName, age, phoneNumber, breed, image, activity});
   })
   .catch( (err) => console.log(err));
 });
 
+// GET     
+siteRouter.get("/delete/:dogId", (req, res) => {
+  Dog.findByIdAndRemove(req.params.dogId)
+    .then(() => res.redirect("/auth/signup-form"))
+    .catch(err => console.log(err));
+});
 
-siteRouter.get('/profile-settings',  (req, res) => {
-  res.render('profile-settings')
+// Render the edit form 
+
+siteRouter.get('/edit',  (req, res) => {
+  const { _id } = req.query;
+
+  Dog.findOne({ _id: _id })
+    .then(oneDog => {
+      const data = {
+        dog: oneDog
+      };
+
+      res.render("profile-settings", data);
+    })
+    .catch(err => console.log(err));
 });
 
 module.exports = siteRouter;
