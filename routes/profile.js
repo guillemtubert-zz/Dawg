c01382cffb5a58499ec04f516b190547f856dd7e
doveriko <abelmorpe@gmail.com>
2020-02-18 15:45:29 +0100

    edit and delete buttons

--- routes/profile.js
+++ routes/profile.js
@@ -18,41 +18,62 @@ siteRouter.use((req, res, next) => {
   }                     
 });
 
+// GET /profile
+
 siteRouter.get('/profile',  (req, res) => {
   const {_id} = req.session.currentUser;
 
-  //doc.find by id to 
   Dog.findOne({_id})
   .then( (dogProfile) => {
-    console.log(dogProfile);
+    console.log("dogProfile", dogProfile);
     
-    const {dogName, age, phoneNumber, breed, image, activity} = dogProfile;
-    res.render('profile', {dogName, age, phoneNumber, breed, image, activity});
+    const {dogName, age, phoneNumber, breed, image, activity, _id} = dogProfile;
+    res.render('profile', {dogName, age, phoneNumber, breed, image, activity, _id});
+    console.log("age", dogName);
   })
   .catch( (err) => console.log(err));
 });
 
-// GET     
-siteRouter.get("/delete/:dogId", (req, res) => {
-  Dog.findByIdAndRemove(req.params.dogId)
-    .then(() => res.redirect("/auth/signup-form"))
+// POST /profile/delete
+
+siteRouter.post("/delete/:id", (req, res) => {
+  
+  Dog.findByIdAndRemove(req.params.id)
+    .then(() => res.redirect("/"))
     .catch(err => console.log(err));
 });
 
-// Render the edit form 
+
+// POST /profile/edit
+
+siteRouter.post('/edit', (req, res) => {
+  const { _id } = req.session.currentUser;
+  const { dogName, age, breed } = req.body;
+
+  Dog.updateOne({_id}, { dogName, age, breed } )
+  .then( () => res.redirect("/profile/profile"))
+  .catch( (err) => console.log(err));
+});
+
+// GET /profile/edit
 
 siteRouter.get('/edit',  (req, res) => {
-  const { _id } = req.query;
+
+  const { _id } = req.session.currentUser;
+  console.log('id', _id);
+  
 
   Dog.findOne({ _id: _id })
     .then(oneDog => {
       const data = {
         dog: oneDog
       };
-
+      console.log(data);
+      
       res.render("profile-settings", data);
     })
     .catch(err => console.log(err));
+
 });
 
 module.exports = siteRouter;
