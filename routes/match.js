686e4448ba8fa127fb6407a5ee246ef5ae4299e4
doveriko <abelmorpe@gmail.com>
2020-04-14 19:45:44 +0200

    minor change to test branch

--- routes/match.js
+++ routes/match.js
@@ -3,131 +3,139 @@ var matchRouter = express.Router();
 const Dog = require("../models/dog.js");
 const Match = require("../models/match.js");
 
-
-
-matchRouter.post('/:id', (req, res, next) =>{
+matchRouter.post("/:id", (req, res, next) => {
   const matchedDog = req.params.id;
 
-  Dog.findById(req.session.currentUser._id).populate('interactions')
-  .then( (dog) => {
-    let alreadyMatched =false
-    if (dog.interactions.length) {
+  Dog.findById(req.session.currentUser._id)
+    .populate("interactions")
+    .then((dog) => {
+      let alreadyMatched = false;
+      if (dog.interactions.length) {
+        for (let i = 0; i < dog.interactions.length; i++) {
+          const interaction = dog.interactions[i];
+          // if (interaction.dogOneId == matchedDog || interaction.dogTwoId == matchedDog ) {
 
-      for (let i = 0; i < dog.interactions.length; i++) {
-        const interaction = dog.interactions[i];
-        // if (interaction.dogOneId == matchedDog || interaction.dogTwoId == matchedDog ) {
-        
-        if (String(interaction.dogOneId) == matchedDog || String(interaction.dogTwoId) == matchedDog ) {
-          alreadyMatched = interaction; 
-          break;
-        } 
+          if (
+            String(interaction.dogOneId) == matchedDog ||
+            String(interaction.dogTwoId) == matchedDog
+          ) {
+            alreadyMatched = interaction;
+            break;
+          }
+        }
+
+        // dog.interactions.forEach((interaction) => {
+
+        //   if (String(interaction.dogOneId) == matchedDog || String(interaction.dogTwoId) == matchedDog ) {
+        //     alreadyMatched = interaction;
+        //   }
+        // })
       }
+      console.log("alreadyMatched", alreadyMatched);
 
-      // dog.interactions.forEach((interaction) => {
+      // if(!alreadyMatched) {
+      //   console.log("Not matched");
 
-        
-        
-      //   if (String(interaction.dogOneId) == matchedDog || String(interaction.dogTwoId) == matchedDog ) {
-      //     alreadyMatched = interaction; 
-      //   }
-      // })
-    }
-    console.log('alreadyMatched', alreadyMatched);
-    
-    // if(!alreadyMatched) {
-    //   console.log("Not matched");
-      
-    //     const dogOneId = req.session.currentUser._id;
-    //     const dogTwoId = matchedDog;
-    //     const dogOneAnswer = req.body.answer;
+      //     const dogOneId = req.session.currentUser._id;
+      //     const dogTwoId = matchedDog;
+      //     const dogOneAnswer = req.body.answer;
 
-    //     Match.create({dogOneId, dogTwoId, dogOneAnswer})
-    //     .then( (createMatch) => {
-    //       Dog.findByIdAndUpdate(
-    //         req.session.currentUser._id,
-    //         {
-    //           $push:{"interactions": createMatch, "selected": matchedDog}
-          
-    //         }
-    //         )
-    //       .then((dog)=>{
-    //         res.redirect("/profile/swipe")
-    //       })
-    //     })
-    //     .catch((err) => console.log(err));
-    // } 
+      //     Match.create({dogOneId, dogTwoId, dogOneAnswer})
+      //     .then( (createMatch) => {
+      //       Dog.findByIdAndUpdate(
+      //         req.session.currentUser._id,
+      //         {
+      //           $push:{"interactions": createMatch, "selected": matchedDog}
 
-    if(!alreadyMatched) {
-      const dogOneId = req.session.currentUser._id;
-      const dogTwoId = matchedDog;
-      const dogOneAnswer = req.body.answer;
+      //         }
+      //         )
+      //       .then((dog)=>{
+      //         res.redirect("/profile/swipe")
+      //       })
+      //     })
+      //     .catch((err) => console.log(err));
+      // }
 
-      Match.create({dogOneId, dogTwoId, dogOneAnswer})
-        .then( (createMatch) => {
-          const { dogTwoId } = createMatch;
+      if (!alreadyMatched) {
+        const dogOneId = req.session.currentUser._id;
+        const dogTwoId = matchedDog;
+        const dogOneAnswer = req.body.answer;
 
-          const pr1 = Dog.findByIdAndUpdate(req.session.currentUser._id, {$push:{ interactions: createMatch }});
-          const pr2 = Dog.findByIdAndUpdate(req.session.currentUser._id, {$push:{ selected: dogTwoId }});
+        Match.create({ dogOneId, dogTwoId, dogOneAnswer })
+          .then((createMatch) => {
+            const { dogTwoId } = createMatch;
 
-          const pr3 = Dog.findByIdAndUpdate(matchedDog ,{$push:{"interactions": createMatch }})
-          const whenBothUpdatedPr = Promise.all([pr1, pr2, pr3]);
-          return whenBothUpdatedPr;
-      })
-      .then((updatedDogs)=>{
-        console.log('UPDATED DOGS OPERATIONS: \n', updatedDogs);
-        
-        res.redirect("/profile/swipe")
-      })
-      .catch((err) => console.log(err));
-    } 
-     else {
-      console.log("Matched");
-      
-      const dogTwoAnswer = req.body.answer;
-      // console.log('data we have:', alreadyMatched, dogTwoAnswer);
+            const pr1 = Dog.findByIdAndUpdate(req.session.currentUser._id, {
+              $push: { interactions: createMatch },
+            });
+            const pr2 = Dog.findByIdAndUpdate(req.session.currentUser._id, {
+              $push: { selected: dogTwoId },
+            });
 
-      let state;
+            const pr3 = Dog.findByIdAndUpdate(matchedDog, {
+              $push: { interactions: createMatch },
+            });
+            const whenBothUpdatedPr = Promise.all([pr1, pr2, pr3]);
+            return whenBothUpdatedPr;
+          })
+          .then((updatedDogs) => {
+            console.log("UPDATED DOGS OPERATIONS: \n", updatedDogs);
 
-      if (alreadyMatched.dogOneAnswer === "like" && dogTwoAnswer === "like"){
-        state = "success";
+            res.redirect("/profile/swipe");
+          })
+          .catch((err) => console.log(err));
       } else {
-        state = "rejected";
-      }
-      console.log({alreadyMatched, dogTwoAnswer, state});
-      
-      
-      Match.findByIdAndUpdate(alreadyMatched._id, {dogTwoAnswer, success: state}, {new:true})
-      .then((updatedMatch)=>{
-        const { dogOneId } = updatedMatch
-        const pr = Dog.findByIdAndUpdate(req.session.currentUser._id, {$push:{ selected: dogOneId }});
-        return pr;
-      })
-      .then(() => {
-        res.redirect("/profile/swipe")
-      })
-    }
-      
-    
-  })
-})
+        console.log("Matched");
 
-matchRouter.get('/',  (req, res) => {
-  const {_id} = req.session.currentUser;
-  Match.find({success:"success", $or:[ {dogOneId: _id}, {dogTwoId: _id}]})
-    .populate("dogOneId dogTwoId") 
-    .then( (matches) => {
-      const filteredMatches = matches.map(matchObj => {
-        if (String(matchObj.dogOneId._id) == String(_id)) {
-          return matchObj.dogTwoId
+        const dogTwoAnswer = req.body.answer;
+        // console.log('data we have:', alreadyMatched, dogTwoAnswer);
+
+        let state;
+
+        if (alreadyMatched.dogOneAnswer === "like" && dogTwoAnswer === "like") {
+          state = "success";
+        } else {
+          state = "rejected";
         }
-        else{
-          return matchObj.dogOneId
-        }
-      })
-      res.render('match', {matches: filteredMatches})
-    })
-    .catch(err => console.log(err)
-    )
+        console.log({ alreadyMatched, dogTwoAnswer, state });
+
+        Match.findByIdAndUpdate(
+          alreadyMatched._id,
+          { dogTwoAnswer, success: state },
+          { new: true }
+        )
+          .then((updatedMatch) => {
+            const { dogOneId } = updatedMatch;
+            const pr = Dog.findByIdAndUpdate(req.session.currentUser._id, {
+              $push: { selected: dogOneId },
+            });
+            return pr;
+          })
+          .then(() => {
+            res.redirect("/profile/swipe");
+          });
+      }
+    });
 });
 
-  module.exports = matchRouter;
+matchRouter.get("/", (req, res) => {
+  const { _id } = req.session.currentUser;
+  Match.find({
+    success: "success",
+    $or: [{ dogOneId: _id }, { dogTwoId: _id }],
+  })
+    .populate("dogOneId dogTwoId")
+    .then((matches) => {
+      const filteredMatches = matches.map((matchObj) => {
+        if (String(matchObj.dogOneId._id) == String(_id)) {
+          return matchObj.dogTwoId;
+        } else {
+          return matchObj.dogOneId;
+        }
+      });
+      res.render("match", { matches: filteredMatches });
+    })
+    .catch((err) => console.log(err));
+});
+
+module.exports = matchRouter;
